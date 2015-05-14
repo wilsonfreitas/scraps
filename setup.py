2b757300329375af7ec3ea4b137c79749e018ec0
Wilson Nascimento de Freitas <wilson.freitas@gmail.com>
2015-05-14 09:13:38 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="scraps",
+    version="0.0.1",
+    py_modules=['scraps'],
+    author='Wilson Freitas',
+    author_email='wilson.freitas@gmail.com',
+    description='Functional web scraping tool',
+    url='https://github.com/wilsonfreitas/scraps',
+    license='MIT',
+    keywords='web scraping, crawler, crawl, scraping',
+)
+
